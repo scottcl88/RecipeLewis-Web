5ee7f78d4e5fccc127c3a9c96711bbbfa8c99bb0
Scott Lewis <scottcl@outlook.com>
2022-05-28 19:52:56 -0400

    Adding Admin page

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -9,12 +9,15 @@ namespace BlazorApp.Services;
 public interface IUserService
 {
     Task<UserModel> GetUser();
+    Task<List<UserModel>> GetAll();
     Task<GenericResult> RequestEditAccess();
     Task<GenericResult> VerifyEmail(VerifyEmailRequest request);
     Task<GenericResult> ForgotPassword(ForgotPasswordRequest request);
     Task<GenericResult> ResetPassword(ResetPasswordRequest request);
     Task<GenericResult> ValidateResetToken(ValidateResetTokenRequest request);
     Task<GenericResult> Register(RegisterRequest request);
+    Task<UserModel> Promote(UserId userId);
+    Task<UserModel> Demote(UserId userId);
 }
 
 public class UserService : IUserService
@@ -29,6 +32,10 @@ public class UserService : IUserService
     public async Task<UserModel> GetUser()
     {
         return await _httpService.Get<UserModel>("users/user");
+    }
+    public async Task<List<UserModel>> GetAll()
+    {
+        return await _httpService.Get<List<UserModel>>("users/get-all");
     }
     public async Task<GenericResult> RequestEditAccess()
     {
@@ -53,5 +60,13 @@ public class UserService : IUserService
     public async Task<GenericResult> Register(RegisterRequest request)
     {
         return await _httpService.Post<GenericResult>("users/register", request);
+    }
+    public async Task<UserModel> Promote(UserId userId)
+    {
+        return await _httpService.Post<UserModel>($"users/promote/{userId.Value}");
+    }
+    public async Task<UserModel> Demote(UserId userId)
+    {
+        return await _httpService.Post<UserModel>($"users/demote/{userId.Value}");
     }
 }
